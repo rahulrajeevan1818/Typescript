afdfb0a84b44ec90412461b4196fbc347ad07f01
rahul <rahul.rajeevan@spericorn.com>
2023-01-28 11:44:10 +0530

    initial setup

--- using-ts.js
+++
@@ -1,9 +0,0 @@
-var btn = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function Add(input1, input2) {
-    return input1 + input1;
-}
-btn.addEventListener("click", function () {
-    console.log(Add(+input1.value, +input1));
-});
